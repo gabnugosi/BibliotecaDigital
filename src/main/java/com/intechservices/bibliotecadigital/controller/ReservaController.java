16c17b93ee22ff69fe929ad1464c70f75ed877e8
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-10-27 16:56:55 -0300

    Inclusão das classes de emprestimo bean e DAO

---
+++ src/main/java/com/intechservices/bibliotecadigital/controller/ReservaController.java
@@ -0,0 +1,15 @@
+package com.intechservices.bibliotecadigital.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.intechservices.bibliotecadigital.model.Reserva;
+
+@RestController
+public class ReservaController {
+	/*
+	@RequestMapping("/reserva")
+	public Reserva reserva() {
+		
+	}*/
+}
