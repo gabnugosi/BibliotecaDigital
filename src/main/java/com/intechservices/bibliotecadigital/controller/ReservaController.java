415cd4bd15a062b6f8dc4f874af3504a45df0981
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-10-27 20:23:46 -0300

    Edição do controller reserva

--- src/main/java/com/intechservices/bibliotecadigital/controller/ReservaController.java
+++ src/main/java/com/intechservices/bibliotecadigital/controller/ReservaController.java
@@ -1,15 +1,17 @@
 package com.intechservices.bibliotecadigital.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.intechservices.bibliotecadigital.model.Reserva;
 
 @RestController
+//@RequestMapping("/api/reserva")
 public class ReservaController {
 	/*
 	@RequestMapping("/reserva")
-	public Reserva reserva() {
-		
+	public Reserva reserva(@RequestParam(value="name",defaultValue="Teste")String name) {
+		return new reserva(String.format("Olá, %s", name));
 	}*/
 }
