dbf4c098be153ff021621a4a0da836ddbec49e3a
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-11-03 22:39:26 -0300

    Alterando o a porta no properties e teste na Controller autor

--- src/main/java/br/com/intechservices/bibliotecadigital/controller/AutorController.java
+++ src/main/java/br/com/intechservices/bibliotecadigital/controller/AutorController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.intechservices.bibliotecadigital.model.Autor;
@@ -21,22 +23,26 @@ import br.com.intechservices.bibliotecadigital.repository.AutorRepository;
 
 
 @RestController
-@RequestMapping("autores")
+@RequestMapping("/autores")
 public class AutorController {
 	@Autowired
 	private AutorRepository autorRepo;
 	
 	 //@RequestMapping(method = RequestMethod.GET)
-	@GetMapping
-	 public ResponseEntity<Autor> GetById(@PathVariable(value = "id") long id)
+	
+	 /*public ResponseEntity<Autor> GetById(@PathVariable(value = "id") int id)
 	    {
 	        Optional<Autor> autor = autorRepo.findById(id);
 	        if(autor.isPresent())
 	            return new ResponseEntity<Autor>(autor.get(), HttpStatus.OK);
 	        else
 	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	    }
-		
+	    }*/
+	@GetMapping
+	 public String listaAutores(@RequestParam("id") int  id, Model model) {
+		 model.addAttribute("id", id);
+	        return "listaAutores";
+	}
 		//@RequestMapping(method =  RequestMethod.POST)
 		@PostMapping
 	    public Autor Post(@Valid @RequestBody Autor autor)
